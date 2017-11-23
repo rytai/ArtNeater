b016761e5844df7964d595063ce2c88d332821d7
Rytai <thompso0@hotmail.com>
2017-11-23 14:12:41 +0200

    Encoder muuttujat ja metodit lisätty. Odottaa luokaksi muuntamista.

--- koodi/koodi.ino
+++ koodi/koodi.ino
@@ -1,6 +1,12 @@
+//LCD
+#include <Wire.h> //I2C kommunikointi
+#include <hd44780.h>//hd44780 ajuri (LCD)
+#include <hd44780ioClass/hd44780_I2Cexp.h> //i2c expander ajuri lcd:lle
+
 //########################## Pin define #####################
 const unsigned int PIN_ENCODER_CLK = 5;  // Connected to CLK on KY-040
 const unsigned int PIN_ENCODER_DT = 4;  // Connected to DT on KY-040
+const unsigned int PIN_ENCODER_BUTTON = 7;  // Connected to DT on KY-040
 
 //########################## Channel Data ###################
 unsigned int channel_start = 0;
@@ -22,7 +28,9 @@ unsigned byte rainbow_fade_speed = 0;
 //########################## Display ########################
 bool display_update_required = true;
 unsigned long lcd_update_next = 0; //ms
-unsigned int lcd_update_frequency = 100; //ms
+const unsigned int lcd_update_frequency = 100; //ms
+const unsigned int LCD_COLUMNS = 16;
+const unsigned int LCD_ROWS = 2;
 
 //########################## Adjustments ####################
 //Kanavilta tulleet rgb arvot kerrotaan tällä. arvo*brightness/100
@@ -32,10 +40,9 @@ unsigned byte brightness = 100; //0-100%
 signed int encoder_movement = 0;
 bool encoder_clk_current = 0;
 bool encoder_clk_last = 0;
-bool encoder_dt_current = 0;
-bool encoder_dt_last = 0;
-bool button_pressed = false;
-bool button_changed = false;
+bool button_pressed_current = false;
+bool button_pressed_last = false;
+bool button_falling_edge = false;
 
 //######################## Networking  ######################
 //udp databuffer etc?
@@ -45,7 +52,7 @@ bool button_changed = false;
 char menu_lcd_projection[16*2];
 //######################## Misclanneous #####################
 unsigned long mainloop_next_update = 0;
-unsigned int mainloop_max_frequency = 1; //ms
+const unsigned int mainloop_max_frequency = 1; //ms
 unsigned long current_time = 0;
 //###########################################################
 
@@ -66,10 +73,53 @@ void setup() {
     mainloop_next_update = millis();
 }
 
+//Päivitä encoderin sijainti ja palauta onko se muuttunut
+bool InputEncoderHasChanged(){
+  encoder_clk_current = digitalRead(PIN_ENCODER_CLK);
+  //Pyörähdys lasketaan vain encoderin clk pinnin laskureunalla.
+  if ( (encoder_clk_current != encoder_clk_last) && !encoder_clk_current){
+    //clk muuttui ensin -> pyörähdettiin myötäpäivään
+    if( digitalRead(PIN_ENCODER_DT) != encoder_clk_current ){
+      encoder_movement += 1;   
+    }else{ // Pyörähdettiin vastapäivään
+      encoder_movement -= 1;
+    }
+      return true;
+  }else{
+      return false;
+  }
+}
+
+bool InputButtonHasChanged(){
+  button_pressed_last = button_pressed_current;
+  button_pressed_current = digitalRead(PIN_ENCODER_BUTTON);
+  if (button_pressed_current != button_pressed_last){
+    if(!button_pressed_current){
+      button_falling_edge = true;
+    }
+    button_falling_edge = false;
+    return true;
+  }
+  return false;
+}
+
 //Päivitetään input laitteiden tila
 //palauttaa true, jos jotain on muuttunut.
 bool UpdateInput(){
-    return true;
+  bool input_changed = false; //Palautetaan funktion lopussa true jos jokin muuttunut
+  if ( InputEncoderHasChanged() ){
+    input_changed = true;
+  }
+  if ( InputButtonHasChanged() ){
+    input_changed = true;
+  }
+  return input_changed;
+}
+
+//Tähän tulee encoderin kiihtyvyyden käsittely
+//Menu luokassa kutsutaan tätä
+signed int GetEncoderMovement(){
+  return encoder_movement;
 }
 
 //Sisältää menutoiminnot
