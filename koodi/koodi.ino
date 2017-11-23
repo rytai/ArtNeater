de14d4ff1001e2881a715f47faeb4a509f53b0b7
Rytai <thompso0@hotmail.com>
2017-11-23 12:10:36 +0200

    Muokattu alkukoodi kattavammaksi. Ei juostu, eli ei välttämättä käänny

--- koodi/koodi.ino
+++ koodi/koodi.ino
@@ -8,16 +8,21 @@ unsigned int channel_width = 1;
 unsigned byte channel_buffer[128*3];
 
 //########################## Modes ##########################
+//0 Default     Artnet in, leds out.
+//1 simpleRPG   Users sets all leds to same R,P,G values
+//2 RainbowFade Colorful demo animation
+unsigned byte operation_mode = 0;
 // simpleRGB
-bool simpleRGB_enable = false;
 unsigned byte simpleRGB_r = 255;
 unsigned byte simpleRGB_g = 255;
 unsigned byte simpleRGB_b = 255;
 // raindbow fade
-bool rainbow_fade_enabled = false;
+unsigned byte rainbow_fade_speed = 0;
 
 //########################## Display ########################
 bool display_update_required = true;
+unsigned long lcd_update_next = 0; //ms
+unsigned int lcd_update_frequency = 100; //ms
 
 //########################## Adjustments ####################
 //Kanavilta tulleet rgb arvot kerrotaan tällä. arvo*brightness/100
@@ -35,32 +40,62 @@ bool button_changed = false;
 //######################## Networking  ######################
 //udp databuffer etc?
 
+//######################## Menu #############################
+//Sisältää menun tuottaman char arrayn, joka pusketaan lcd-ruudulle.
+char menu_lcd_projection[16*2];
+//######################## Misclanneous #####################
+unsigned long mainloop_next_update = 0;
+unsigned int mainloop_max_frequency = 1; //ms
+unsigned long current_time = 0;
 //###########################################################
 
 void setup() { 
- pinMode (PIN_ENCODER_CLK,INPUT);
- pinMode (PIN_ENCODER_DT,INPUT);
- encoder_dt_last = digitalRead(PIN_ENCODER_DT);
+    //Alustetaan GPIO pinnit
+    pinMode (PIN_ENCODER_CLK,INPUT);
+    pinMode (PIN_ENCODER_DT,INPUT);
 
- Serial.begin (9600);
- Serial.println("Starting.");
+    //Alustetaan laitteisiin liittyvät muuttujat
+    encoder_dt_last = digitalRead(PIN_ENCODER_DT);
+
+    //Aloitetaan serial debugging
+    Serial.begin (9600);
+    Serial.println("Starting.");
+
+    //Alustetaan ajanotot
+    lcd_update_next = millis();
+    mainloop_next_update = millis();
 }
 
-//Päivitetään input laitteiden tila ja palautetaan true,
-//jos jotain on muuttunut.
+//Päivitetään input laitteiden tila
+//palauttaa true, jos jotain on muuttunut.
 bool UpdateInput(){
     return true;
 }
 
+//Sisältää menutoiminnot
+//Palauttaa true, jos menu on muuttunut -> seuraa lcd päivitys
+bool UpdateMenu(){
+    return true;
+}
+
+void UpdateLCD(){
+}
+
 void loop() {
-    //Tähän loopin nopeuden säätö
+    //Testataan onko kulunut tarpeeksi aikaa viimeisestä loopin läpikäynnistä
+    current_time = millis();
+    if (current_time >= mainloop_next_update){
+        mainloop_next_update = current_time + mainloop_max_frequency;
 
-    if (UpdateInput()){
-        UpdateMenu();
+        //Testataan syöttölaitteet ja päivitetään näyttö jos jokin syöte on muuttunut
+        if ( UpdateInput() ){
+            //Merkataan lcd näyttö päivitettäväksi, jos menu on muuttunut
+            display_update_required = UpdateMenu();
+        }
+        //Mikäli tarpeeksi aikaa on kulunut viime lcd päivityksestä -> päivitetään lcd
+        if ( display_update_required && (millis() >= lcd_update_next) ){
+            lcd_update_next += lcd_update_frequency;
+            UpdateLCD();
+        }
     }
-
-    if ( display_update_required ){
-        //jos tarpeeksi aikaa kulunut viime lcd päivityksestä, päivitetään lcd.
-    }
-       
 } 
